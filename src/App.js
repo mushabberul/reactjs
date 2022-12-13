e245fba8fc134da33b27d160b1c201588193d4be
mushabberul <dev.sabbirmia@gmail.com>
2022-12-13 11:17:10 +0600

    create class component, declear state, and show state data by looping

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import './App.css';
-import Header from './components/Header';
+// function component not support state
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    siteTitle: 'This is sitetitle',
+    products: [
+      {
+        id: 1,
+        name: "product 1",
+        desc: "product 1 description"
+      },
+      {
+        id: 2,
+        name: "product 2",
+        desc: "product 2 description"
+      },
+      {
+        id: 3,
+        name: "product 3",
+        desc: "product 3 description"
+      },
+    ]
+  }
+  render() {
+    console.log(this.state.products)
+
+    return (
+      <div>
+        <h1>{this.state.siteTitle}</h1>
+        <h2>Product</h2>
+        {
+          this.state.products.map((product) => (
+            <li key={product.id}>Id: {product.id}, Name: {product.name}, Description: {product.desc}</li>
+          ))
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+
